a005b93f799d69df1a25fe1dbd9ac52c6cb4ad59
kouta <takeuchi180121@gmail.com>
2025-02-14 18:01:41 +0900

    feat: drizzle用にファイルを変更

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Client, GatewayIntentBits} from "discord.js";
 import dotenv from "dotenv";
 import { registerCommands } from "./commands"; 
-import { registerEvents } from "./events";
+import { registerEvents } from "./events"
+import { db } from "./db";
+import { schedules } from "./db/schema";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 
 dotenv.config();
 
@@ -13,7 +16,16 @@ const client = new Client({
   ],
 });
 
+const main = async () => {
+  const result = await db.select().from(schedules);
+  console.log(result);
+}
+
+migrate(db, { migrationsFolder: "./src/db/migrations" });
 registerCommands(client);
 registerEvents(client);
+
+main();
+
 
 client.login(process.env.TOKEN);
