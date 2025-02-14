b349a01444cd65941e6caecdcfcb92846c1d2f8e
kouta <takeuchi180121@gmail.com>
2025-02-14 18:12:39 +0900

    refact: スペースを調整

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,4 @@ import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 
 const sqlite = new Database("./src/db/database.db");
-export const db: BetterSQLite3Database= drizzle(sqlite);
-
+export const db: BetterSQLite3Database = drizzle(sqlite);
