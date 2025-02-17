ba79b10c4ce19f767dd4f60460cad4b777ac59a2
kouta <takeuchi180121@gmail.com>
2025-02-18 06:26:45 +0900

    feat: 型を定義

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,20 +1,36 @@
-export type Stage = {
+export type TStage = {
   id: number;
   name: string;
   image: string;
 }
 
-export type MatchData = {
-  startTime: string;
-  endTime: string;
+export type TMatchData = {
+  start_time: string;
+  end_time: string;
   rule: {
     key: string;
     name: string;
   };
-  stages: Stage[]; 
+  stages: TStage[]; 
   isFest: boolean;
 }
 
-export type FetchData = {
-  
+export type TFetchData = {
+  result: {
+    regular: TMatchData[];
+    bankara_open: TMatchData[];
+    bankara_challenge: TMatchData[];
+    x: TMatchData[];
+    event: any;
+    fest: any;
+  }
+}
+
+export type TSchedule = {
+  startTime: string;
+  endTime: string;
+  matchTypeKey: string;
+  ruleKey: string;
+  stage1ID: number;
+  stage2ID: number;
 }
