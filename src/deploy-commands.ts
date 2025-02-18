387d11a7555d24fe50e26b4dd36e617ce0f9857d
kouta <takeuchi180121@gmail.com>
2025-02-18 22:17:35 +0900

    feat: searchを追加

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -2,14 +2,13 @@ import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import dotenv from "dotenv";
 
 import { ping } from "./bot/commands/ping";
-import { hello } from "./bot/commands/hello";
-import { talk } from "./bot/commands/talk";
 import { embed } from "./bot/commands/embed";
-import { fetchApi } from "./bot/commands/fetch-api";
+import { choice } from "./bot/commands/choice";
+import { search } from "./bot/commands/search";
 
 dotenv.config();
 
-const commands = [embed, ping, hello, talk, fetchApi].map((command) => command.data.toJSON());
+const commands = [embed, ping, choice, search].map((command) => command.data.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN as string);
 
