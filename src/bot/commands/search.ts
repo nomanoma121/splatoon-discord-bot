8e6b32a8968a6cd75280851be70fb45b40ab40e3
kouta <takeuchi180121@gmail.com>
2025-02-20 22:12:01 +0900

    feat: 結果表示を実装

--- src/bot/commands/search.ts
+++ src/bot/commands/search.ts
@@ -6,6 +6,79 @@ import {
 import { seeds } from "../../db/seed-data";
 import { Schedules } from "../../db/queries";
 
+type Match = {
+  startTime: string;
+  rule: string;
+  stage1: string;
+  stage2: string;
+};
+
+type FormatedResults = {
+  regular: Match[];
+  bankaraOpen: Match[];
+  bankaraChallenge: Match[];
+  x: Match[];
+};
+
+// TODO: 型をちゃんと定義する
+const formatResults = (results: any) => {
+  const formatedResults: FormatedResults = {
+    regular: [],
+    bankaraOpen: [],
+    bankaraChallenge: [],
+    x: [],
+  };
+
+  results.forEach((result: any) => {
+    const match = {
+      startTime: result.schedules.startTime,
+      rule: result.rules.name,
+      stage1: result.stage1.name,
+      stage2: result.stage2.name,
+    };
+
+    if (result.match_types.key === "regular") {
+      formatedResults.regular.push(match);
+    } else if (result.match_types.key === "bankara_open") {
+      formatedResults.bankaraOpen.push(match);
+    } else if (result.match_types.key === "bankara_challenge") {
+      formatedResults.bankaraChallenge.push(match);
+    } else {
+      formatedResults.x.push(match);
+    }
+  });
+
+  return formatedResults;
+};
+
+const errEmbed = (text: string) => {
+  return new EmbedBuilder()
+    .setColor("#ff0000")
+    .setTitle("エラー")
+    .setDescription(text)
+    .setImage("https://img.atwiki.jp/dmps_fun/pub/ICON/20012001/SPECIAL01.png")
+};
+
+const formatDate = (date: string) => {
+  const d = new Date(date);
+  return `${d.getMonth() + 1}/${d.getDate()} ${d.getHours()}:${d.getMinutes().toString().padStart(2, "0")}`;
+}
+
+const keyToName = (key: string) => {
+  switch (key) {
+    case "regular":
+      return "レギュラーマッチ";
+    case "bankaraOpen":
+      return "バンカラオープン";
+    case "bankaraChallenge":
+      return "バンカラチャレンジ";
+    case "x":
+      return "Xマッチ";
+    default:
+      return key;
+  }
+};
+
 export const search = {
   name: "search",
   data: new SlashCommandBuilder()
@@ -49,49 +122,36 @@ export const search = {
     const rule = interaction.options.getString("ルール") ?? null;
     const matchType = interaction.options.getString("バトル形式") ?? null;
 
+    // スケジュールを検索
     const results = await Schedules.search(stage, rule, matchType);
+    const formatedResults = formatResults(results);
 
-    const errEmbed = (text: string) => {
-      return new EmbedBuilder()
-      .setColor("#ff0000")
-      .setTitle("エラー")
-      .setDescription(text)
-      .setImage("https://img.atwiki.jp/dmps_fun/pub/ICON/20012001/SPECIAL01.png")
-      .setTimestamp();
-    }
-
-    if (!results) {
-      await interaction.reply({ embeds: [errEmbed("データベースにエラーが発生しました")] });
-      return;
-    }
-
-    if (results.length === 0) {
-      await interaction.reply({ embeds: [errEmbed("検索結果が見つかりませんでした")] });
-      return;
-    }
-
-    if (results.length > 25) {
-      await interaction.reply({ embeds: [errEmbed("検索結果が多すぎます")] });
-      return;
-    }
+    Object.values(formatedResults).forEach(async (result) => {
+      if (!result) {
+        await interaction.reply({
+          embeds: [errEmbed("データベースにエラーが発生しました")],
+        });
+        return;
+      }
+    });
 
     const embed = new EmbedBuilder()
       .setColor("#0099ff")
-      .setTitle("検索結果")
+      .setTitle("検索結果をおしらせします")
       .setDescription(
         `検索キーワード: ${stage ?? ""}, ${rule ?? ""}, ${matchType ?? ""}`
       )
-      .setTimestamp();
 
-    results.forEach((result) => {
+    Object.entries(formatedResults).forEach(([key, value]) => {
       embed.addFields([
-        { name: "開始時間", value: result.schedules.startTime, inline: true },
-        { name: "終了時間", value: result.schedules.endTime, inline: true },
-        { name: "ルール", value: result.rules.name ?? "不明", inline: true },
         {
-          name: "バトル形式",
-          value: result.match_types.name ?? "不明",
-          inline: true,
+          name: `**${keyToName(key)}**`,
+          value: value
+            .map(
+              (v: any) =>
+                `◦${formatDate(v.startTime)} ~ \n${v.rule} ${v.stage1}, ${v.stage2}`
+            )
+            .join("\n"),
         },
       ]);
     });
