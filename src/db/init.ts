d2616768c2898313de3860165f31b414aa4e84db
kouta <takeuchi180121@gmail.com>
2025-02-20 22:27:16 +0900

    fix: migration時にエラーを吐いてしまう問題を修正

--- src/db/init.ts
+++ src/db/init.ts
@@ -6,49 +6,53 @@ import { seeds } from "./seed-data";
 export const initializeDB = async () => {
   console.log("Starting DB initialization...");
 
-  await Schedules.deleteAll().then(() => {
-    Schedules.getAll().then((schedules) => {
-      console.log(schedules);
-    });
-  }).then(() => {
-    console.log("Schedules deleted");
+  migrate(db, {
+    migrationsFolder: "./src/db/migrations",
   });
 
-  migrate(db, { migrationsFolder: "./src/db/migrations" });
-  
- try {
-  if ((await Stages.getAll()).length === 0) {
-    seeds.stages.forEach((stage) => {
-      Stages.create(stage);
+  await Schedules.deleteAll()
+    .then(() => {
+      Schedules.getAll().then((schedules) => {
+        console.log(schedules);
+      });
+    })
+    .then(() => {
+      console.log("Schedules successfully deleted");
     });
-  }
 
-  if ((await Stages.getAll()).length !== seeds.stages.length) {
-    throw new Error("Stages not seeded correctly!");
-  }
+  try {
+    if ((await Stages.getAll()).length === 0) {
+      seeds.stages.forEach((stage) => {
+        Stages.create(stage);
+      });
+    }
 
-  if ((await MatchTypes.getAll()).length === 0) {
-    seeds.matchTypes.forEach((matchType) => {
-      MatchTypes.create(matchType);
-    });
-  }
+    if ((await Stages.getAll()).length !== seeds.stages.length) {
+      throw new Error("Stages not seeded correctly!");
+    }
 
-  if ((await MatchTypes.getAll()).length !== seeds.matchTypes.length) {
-    throw new Error("MatchTypes not seeded correctly!");
-  }
+    if ((await MatchTypes.getAll()).length === 0) {
+      seeds.matchTypes.forEach((matchType) => {
+        MatchTypes.create(matchType);
+      });
+    }
 
-  if ((await Rules.getAll()).length === 0) {
-    seeds.rules.forEach((rule) => {
-      Rules.create(rule);
-    });
-  }
+    if ((await MatchTypes.getAll()).length !== seeds.matchTypes.length) {
+      throw new Error("MatchTypes not seeded correctly!");
+    }
 
-  if ((await Rules.getAll()).length !== seeds.rules.length) {
-    throw new Error("Rules not seeded correctly!");
+    if ((await Rules.getAll()).length === 0) {
+      seeds.rules.forEach((rule) => {
+        Rules.create(rule);
+      });
+    }
+
+    if ((await Rules.getAll()).length !== seeds.rules.length) {
+      throw new Error("Rules not seeded correctly!");
+    }
+  } catch (err) {
+    console.error(err);
   }
-} catch (err) {
-  console.error(err);
-}
 
   console.log("DB initialization complete!");
 };
