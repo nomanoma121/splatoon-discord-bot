34e6abe5ec35f7c591facac989567aeb610b8c04
kouta <takeuchi180121@gmail.com>
2025-02-24 10:06:45 +0900

    feat: 次と現在のスケジュールを取得するクエリを実装

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,7 +1,7 @@
 import { db } from "./index";
 import { schedules, stages, rules, matchTypes } from "./schema";
 import { SQLiteTable } from "drizzle-orm/sqlite-core";
-import { eq, and, or, aliasedTable, desc, asc } from "drizzle-orm";
+import { eq, and, or, gt, aliasedTable, desc, asc, max } from "drizzle-orm";
 import { TSchedule } from "../utils/types";
 
 const getAll = async (table: SQLiteTable) => {
@@ -12,8 +12,13 @@ const create = async (table: SQLiteTable, values: any) => {
   return await db.insert(table).values(values);
 };
 
-const deleteAll = async (table: SQLiteTable) => {
-  return await db.delete(table);
+const clean = async () => {
+  const maxStartTime = await db
+    .select({ value: max(schedules.startTime) })
+    .from(schedules) as { value: string }[];
+  return await db
+    .delete(schedules)
+    .where(gt(schedules.startTime, maxStartTime[0].value));
 };
 
 const search = async (
@@ -35,7 +40,7 @@ const search = async (
       and(
         stage ? or(eq(stage1.name, stage), eq(stage2.name, stage)) : undefined,
         rule ? eq(rules.name, rule) : undefined,
-        matchType ? eq(matchTypes.name, matchType) : undefined 
+        matchType ? eq(matchTypes.name, matchType) : undefined
       )
     );
 };
@@ -44,28 +49,67 @@ const getTimeRange = async () => {
   const min = await db
     .select()
     .from(schedules)
-    .orderBy(
-      asc(schedules.startTime)
-    )
+    .orderBy(asc(schedules.startTime));
   const max = await db
     .select()
     .from(schedules)
-    .orderBy(
-      desc(schedules.startTime)
-    )
+    .orderBy(desc(schedules.startTime));
   return {
     min: min[0].startTime,
-    max: max[0].startTime
-  }
-}
+    max: max[0].startTime,
+  };
+};
 
+const currentSchedules = async () => {
+  const now = new Date();
+  // 現在の時間が奇数ならそのまま、偶数なら1引く
+  const beginHour =
+    now.getHours() % 2 === 0 ? now.getHours() - 1 : now.getHours();
+  const begin = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    beginHour,
+    0,
+    0
+  ).toString();
+  return await db
+    .select()
+    .from(schedules)
+    .where(eq(schedules.startTime, begin));
+};
+
+const nextSchedules = async () => {
+  const now = new Date();
+  const beginHour =
+    now.getHours() % 2 === 0 ? now.getHours() + 1 : now.getHours() + 2;
+  const begin = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    beginHour,
+    0,
+    0
+  ).toString();
+  return await db
+    .select()
+    .from(schedules)
+    .where(eq(schedules.startTime, begin));
+};
+  
 export const Schedules = {
   getAll: () => getAll(schedules),
   create: (data: TSchedule) => create(schedules, data),
-  search: (stage: string | null, rule: string | null, matchType: string | null) =>
-    search(stage, rule, matchType),
+  search: (
+    stage: string | null,
+    rule: string | null,
+    matchType: string | null
+  ) => search(stage, rule, matchType),
+  current: () => currentSchedules(),
+  next: () => nextSchedules(),
   getTimeRange: () => getTimeRange(),
-  deleteAll: () => deleteAll(schedules),
+  clean: () => clean(),
+  deleteAll: () => db.delete(schedules),
 };
 
 export const Stages = {
